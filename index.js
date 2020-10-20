09f311f272e4e29f352bdd87eeb8dddc9761171b
AndreaDeGuzman01 <andreamauricedeguzman@student.laverdad.edu.ph>
2020-10-20 22:59:43 +0800

    updated

---
+++ index.js
@@ -0,0 +1,15 @@
+function validateForm() {
+  var name = document.getElementById('guest-name').value 
+  var email = document.getElementById('guest-email').value
+  var mes = document.getElementById('guest-mes')
+ var text;
+ // validate if empty or not
+  if (name == "" || email == "" || mes == "") {
+    text = "Please fill out the form completely.";
+    } 
+  else {
+    text = "Thank you " + name + " " + email + ", we received your message.";
+    }
+// displays text 
+    alert(text);
+}
